65248f415bd7cae1828208023a8f6dcc4160338a
Fran <Fran@DESKTOP-NO101JL>
2020-07-14 17:24:44 +0200

    display list of recipes

--- src/main/java/com/lucidchip/recipemvc/controllers/IndexController.java
+++ src/main/java/com/lucidchip/recipemvc/controllers/IndexController.java
@@ -3,8 +3,11 @@ package com.lucidchip.recipemvc.controllers;
 import com.lucidchip.recipemvc.domain.Category;
 import com.lucidchip.recipemvc.domain.UnitOfMeasure;
 import com.lucidchip.recipemvc.repositories.CategoryRepository;
+import com.lucidchip.recipemvc.repositories.RecipeRepository;
 import com.lucidchip.recipemvc.repositories.UnitOfMeasureRepository;
+import com.lucidchip.recipemvc.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,22 +15,16 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+   private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription(("Teaspoon"));
-
-        System.out.println("category is " + categoryOptional.get().getId());
-        System.out.println("UOM is " + unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
---
+++ src/main/java/com/lucidchip/recipemvc/services/RecipeService.java
@@ -0,0 +1,10 @@
+package com.lucidchip.recipemvc.services;
+
+import com.lucidchip.recipemvc.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
