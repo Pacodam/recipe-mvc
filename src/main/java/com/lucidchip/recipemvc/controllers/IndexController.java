12aeabba720148085077b394970904b2750b1ad3
Fran <Fran@DESKTOP-NO101JL>
2020-08-03 17:30:00 +0200

    Bootstrap CSS

--- src/main/java/com/lucidchip/recipemvc/controllers/IndexController.java
+++ src/main/java/com/lucidchip/recipemvc/controllers/IndexController.java
@@ -1,23 +1,19 @@
 package com.lucidchip.recipemvc.controllers;
 
-import com.lucidchip.recipemvc.domain.Category;
-import com.lucidchip.recipemvc.domain.UnitOfMeasure;
-import com.lucidchip.recipemvc.repositories.CategoryRepository;
-import com.lucidchip.recipemvc.repositories.RecipeRepository;
-import com.lucidchip.recipemvc.repositories.UnitOfMeasureRepository;
 import com.lucidchip.recipemvc.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
 @Controller
+@Slf4j
 public class IndexController {
 
    private final RecipeService recipeService;
 
     public IndexController(RecipeService recipeService) {
+        log.debug("recipe service created");
         this.recipeService = recipeService;
     }
 
