77cb322935b055c13b3beaa6d0b10097d482cbb9
Fran <Fran@DESKTOP-NO101JL>
2020-08-07 18:31:39 +0200

    Bootstrap and Jquery webJars

---
+++ src/test/java/com/lucidchip/recipemvc/domain/CategoryTest.java
@@ -0,0 +1,34 @@
+package com.lucidchip.recipemvc.domain;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @org.junit.Test
+    public void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @org.junit.Test
+    public void getDescription() {
+        String description = "Chinese";
+        category.setDescription(description);
+        assertEquals(description, category.getDescription());
+    }
+
+    @org.junit.Test
+    public void getRecipes() {
+
+    }
+}
